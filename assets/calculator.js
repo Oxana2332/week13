8d4112a2355caa4659812e069d6c372a7bcec325
Oxana2332 <far_north@ro.ru>
2020-12-16 17:55:55 +0400

    добавила простенький калькулятор

---
+++ assets/calculator.js
@@ -0,0 +1,27 @@
+function plusFunction() 
+{
+    let first = prompt (`первое число`);
+    let second = prompt (`второе число`);
+    alert (Number(first) + Number(second));
+}
+
+function minusFunction() 
+{
+    let first = prompt (`первое число`);
+    let second = prompt (`второе число`);
+    alert (Number(first) - Number(second));
+}
+
+function multiplyFunction() 
+{
+    let first = prompt (`первое число`);
+    let second = prompt (`второе число`);
+    alert (Number(first) * Number(second));
+}
+
+function divideFunction() 
+{
+    let first = prompt (`первое число`);
+    let second = prompt (`второе число`);
+    alert (Number(first) / Number(second));
+}
