f88347eccde29692172680ce213211d510ea6e8c
Oxana2332 <far_north@ro.ru>
2020-12-20 14:55:19 +0400

    переписала через return

--- assets/calculator.js
+++ assets/calculator.js
@@ -2,26 +2,48 @@ function plusFunction()
 {
     let first = prompt (`первое число`);
     let second = prompt (`второе число`);
-    alert (Number(first) + Number(second));
+    function plusFunctionplus (first, second)
+    {
+        return (+first + +second);
+    }
+    let result = plusFunctionplus (first, second);
+    alert (result);
 }
+
+
 
 function minusFunction() 
 {
     let first = prompt (`первое число`);
     let second = prompt (`второе число`);
-    alert (first - second);
+    function minusFunctionminus (first, second)
+    {
+        return (first - second);
+    }
+    let result = minusFunctionminus (first, second);
+    alert (result);
 }
 
 function multiplyFunction() 
 {
     let first = prompt (`первое число`);
     let second = prompt (`второе число`);
-    alert (first * second);
+    function multiplyFunctionmultiply (first, second)
+    {
+        return (first * second);
+    }
+    let result = multiplyFunctionmultiply (first, second);
+    alert (result);
 }
 
 function divideFunction() 
 {
     let first = prompt (`первое число`);
     let second = prompt (`второе число`);
-    alert (first / second);
+    function divideFunctiondivide (first, second)
+    {
+        return (first / second);
+    }
+    let result = divideFunctiondivide (first, second);
+    alert (result);
 }
